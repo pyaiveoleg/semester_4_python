1bd807e77670c84cec6e17ab3ff5dfdff7b8b471
pyaiveoleg <pyaive.oleg@yandex.ru>
2021-03-02 23:09:41 +0300

    Fixed according to review

--- homeworks/homework_1/task_1/matrix.py
+++ homeworks/homework_1/task_1/matrix.py
@@ -9,11 +9,12 @@ class Matrix:
     __columns: int
 
     def __init__(self, matrix: List[List[float]], eps=0.01):
-        if len(matrix) != 0:
-            row_length = len(matrix[0])
-            for row in matrix:
-                if len(row) != row_length:
-                    raise ValueError("Rows in a matrix should be equal length")
+        if not matrix or len(matrix[0]) == 0:
+            raise ValueError("Matrix cannot be empty")
+        row_length = len(matrix[0])
+        for row in matrix:
+            if len(row) != row_length:
+                raise ValueError("Rows in a matrix should be of equal length")
         self.eps = eps
         self.__rows = len(matrix)
         self.__columns = len(matrix[0])
@@ -21,25 +22,24 @@ class Matrix:
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Matrix):
-            return NotImplemented
-
-        for i in range(self.__rows):
-            for j in range(self.__columns):
-                if (self.__matrix[i][j] - other.__matrix[i][j]) > self.eps:
-                    return False
-        return True
+            raise TypeError("Incompatible types")
+        return all(
+            (self.__matrix[i][j] - other.__matrix[i][j]) < self.eps
+            for i in range(self.__rows)
+            for j in range(self.__columns)
+        )
 
     def transpose(self) -> "Matrix":
         return Matrix(list(map(list, zip(*self.__matrix))))
 
     def __add__(self, other: "Matrix") -> "Matrix":
         if self.__rows != other.__rows or self.__columns != other.__columns:
-            raise ValueError("Both dimensions of matrices should be equal")
+            raise ValueError("Corresponding dimensions of matrices should be equal")
         return Matrix([[x + y for x, y in zip(self.__matrix[i], other.__matrix[i])] for i in range(self.__rows)])
 
     def __mul__(self, other: "Matrix") -> "Matrix":
         if self.__rows != other.__columns or self.__columns != other.__rows:
-            raise ValueError("Both dimensions of matrices should be equal")
+            raise ValueError("Matrices dimensions are incompatible for multiplying")
         return Matrix(
             [
                 [
--- homeworks/homework_1/task_1/vector.py
+++ homeworks/homework_1/task_1/vector.py
@@ -6,6 +6,8 @@ class Vector:
     __coordinates: List[float]
 
     def __init__(self, coordinates: List[float]):
+        if not coordinates:
+            raise ValueError("The vector must contain at least 1 element")
         self.__coordinates = coordinates
 
     def length(self) -> float:
@@ -14,7 +16,7 @@ class Vector:
     def scalar_product(self, other: "Vector") -> float:
         if len(self.__coordinates) != len(other.__coordinates):
             raise ValueError("Dimensions of vectors aren't equal")
-        return sum(x_i * y_i for x_i, y_i in zip(self.__coordinates, other.__coordinates))
+        return sum(x * y for x, y in zip(self.__coordinates, other.__coordinates))
 
     def angle(self, other: "Vector") -> float:
         return degrees(acos(self.scalar_product(other) / (self.length() * other.length())))
